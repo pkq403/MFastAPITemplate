64df2391eb4245f487e3e09a4cff77b7cdfbd57a
pkq403 <pedrocastrocutillas@gmail.com>
2025-01-27 01:04:25 +0100

    primary key constraint + imports in modules

--- generator.py
+++ generator.py
@@ -9,33 +9,45 @@ script work, the foreign key cols should be always <table-name>_id
 '''
 import sys
 import re
+import os
 import json
 
 
 class Generator:
 
-    def __init__(self, file_path: str):
-        self.file = file_path
+    def __init__(self, file_path: str, project_path: str):
         self.db = {}
-        self.db["imports"] = {"sqlalchemy": set([])} # add here any module u will need in future generationo
+        self.file = file_path
+        self.proyect_path  = project_path
+        # Dynamic content
+        self.api_path = os.path.join(project_path, 'api')
+        self.controllers_path = os.path.join(project_path, self.api_path, 'controllers')
+        self.schemas_path = os.path.join(project_path, 'schemas')
+        self.models_path = os.path.join(project_path, 'models')
+        self.repos_paht = os.path.join(project_path, 'repositories')
+        # Static content (config, auth, tests...)
+
+        
     
-    def _add_new_import(self, module: str, importing: str):
-        self.db["imports"]["sqlalchemy"].add(importing)
-    def _parse_col_type(self, col_type: str):
+    def _add_new_import(self, table: str,  module: str, importing: str):
+        self.db[table]["imports"][module].add(importing)
+
+    def _parse_col_type(self, tablename: str, col_type: str):
         if col_type == "integer":
-            self._add_new_import("sqlalchemy", "Integer")
+            self._add_new_import(tablename, "sqlalchemy", "Integer")
             return "Integer"
         if "varchar" in col_type:
-            self._add_new_import("sqlalchemy", "String")
+            self._add_new_import(tablename, "sqlalchemy", "String")
             return "String({})".format(re.search(r'varchar\((\d+)\)', col_type).group(1))
 
     def _sql_file_reader(self) -> dict:
-        def obtain_table_name(cad): return cad.split(" ")[2].strip()
+        def obtain_tablename(cad): return cad.split(" ")[2].strip()
         with open(self.file, 'r') as file:
             for line in file:
                 if "CREATE TABLE" in line:
-                    table_name = obtain_table_name(line)
-                    self.db[table_name] = {}
+                    tablename = obtain_tablename(line)
+                    self.db[tablename] = {}
+                    self.db[tablename]["imports"] = {"sqlalchemy": set(["Column"])}
                     file.readline()
                     for line in file:
                         if line.startswith(');'):
@@ -44,31 +56,35 @@ class Generator:
                             # guarrada mirar como hacer mejor
                             p_keys = line[15:-2].split(', ')
                             for key in p_keys:
-                                self.db[table_name][key].append("primary_key=True")
+                                self.db[tablename][key].append("primary_key=True")
                         else:
                             splitted_col_line = line.split()
                             col_name = splitted_col_line[0]
                             col_type = splitted_col_line[1]
                             nullable = True if splitted_col_line[2] != "NOT" else False
-                            attribs = [self._parse_col_type(col_type)]
+                            attribs = [self._parse_col_type(tablename, col_type)]
                             if not nullable:
                                 attribs.append("nullable=False")
-                            self.db[table_name][col_name] = attribs
+                            self.db[tablename][col_name] = attribs
                             
 
-                # just understands foreign key with ids of other tables (just useful for intersection tables)
+                # just for foreign key with ids of other tables (just useful for intersection tables)
                 if "ALTER TABLE" in line:
-                    table_name = obtain_table_name(line)
+                    tablename = obtain_tablename(line)
                     splitted_fk = file.readline().split()[2].split('_')
                     to_index = splitted_fk.index('TO')
                     first_table = '_'.join(splitted_fk[1:to_index])
                     second_table = '_'.join(splitted_fk[to_index+1:])
                     self.db[second_table][first_table +"_id"].append("ForeignKey(" + first_table + ".id)")
             
-            # TODO: checks if there is any table with 2 primary keys so that property should be deleted
-            # an added a composed primary key constraint with those variables
             for tablename in self.db:
-                pass
+                table = self.db[tablename]
+                pk = [col for col in table if "primary_key=True" in table[col]]
+                if len(pk) > 1:
+                    pk_constaint_str = "PrimaryKeyConstraint("
+                    for k in pk:
+                        pk_constaint_str += k.replace('_', '.') + ','
+                    table['__table_args__'] = [pk_constaint_str[:-1] + ')']
 
     def _build_models(self):
         '''
@@ -77,32 +93,37 @@ class Generator:
         '''
         ident = 1
         blanks = "".join([" " for i in range(4 * ident)])
-        imports = """
-        from ..config.db.databse_setup import Base
-        from sqlalchemy import sqlalchemyimports 
-        """
-        usedSQLalchemyImports = []
-        def take_varchar_len(x): return int(
-            re.search(r'varchar\((\d+)\)', x).group(1))
-        
+        # 1. create models dir
+        os.makedirs(self.models_path, exist_ok=True)
         # 2.  create the rest of files
         for tablename, cols in self.db.items():
-            with open(tablename + '.py', 'w') as file:
-                # Create sql Model
-                classFormatedName = ''.join(
-                    [tok.capitalize() for tok in tablename.split('_')])
-                file.write(f"class {classFormatedName}(Base):\n")
-                for col, col_attribs in cols.items():
-                    sqlalchemy_col = blanks + f"{col} = Column("
-                    for i in range(len(col_attribs)):
-                        sqlalchemy_col += col_attribs[i]
-                        if i != len(col_attribs) - 1:
-                            sqlalchemy_col += ", "
-                    sqlalchemy_col += ")\n"
-                    file.write(sqlalchemy_col)
-                file.write("")
+            writelines = []
+            # Create sql Model
+            classFormatedName = ''.join(
+                [tok.capitalize() for tok in tablename.split('_')])
+            writelines.append(f"class {classFormatedName}(Base):\n")
+            for col, col_attribs in cols.items():
+                if col == "imports":
+                    continue 
+                if col == "__table_args__":
+                    writelines.append("\n")
+                sqlalchemy_col = blanks + f"{col} = Column("
+                for i in range(len(col_attribs)):
+                    sqlalchemy_col += col_attribs[i]
+                    if i != len(col_attribs) - 1:
+                        sqlalchemy_col += ", "
+                sqlalchemy_col += ")\n"
+                writelines.append(sqlalchemy_col)
+            table_imports = []
+            for i, module in enumerate(cols["imports"]):
+                import_lst = ', '.join(cols["imports"][module])
+                import_str = f"from {module} import {import_lst}\n"
+                if i == len(cols["imports"]) - 1: # dirty, no need to check every iteration, lazy, look to improve
+                    import_str+='\n'
+                writelines.insert(0,import_str)
 
-        # 3. adding import to sqlalchemy model file
+            with open(os.path.join(self.models_path, tablename + '.py'), 'w') as file:
+                file.writelines(writelines)
 
     def generateFastAPItemplate(self):
         self._sql_file_reader()
@@ -111,5 +132,5 @@ class Generator:
 
 
 if __name__ == '__main__':
-    gen = Generator(sys.argv[1])
+    gen = Generator(sys.argv[1], sys.argv[2])
     gen.generateFastAPItemplate()
